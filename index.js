29daa3da7af2584ab12ffcdd57eebcad105fc0ce
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2023-10-09 19:37:04 +0600

    server connected jewelry data in mongodb

--- index.js
+++ index.js
@@ -28,12 +28,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
+
+
+        const jewelrysCollection = client.db("moonJewelry").collection("jewelrys");
+
+
+        app.get('/jewelrys', async(req, res) =>{
+            const result = await jewelrysCollection.find().toArray();
+            res.send(result)
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
